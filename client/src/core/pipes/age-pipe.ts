3cd632e000a0c8848ed5da8a7951f5e7e20ceee4
WeeRG91 <126579703+WeeRG91@users.noreply.github.com>
2026-02-06 11:55:35 +0100

    Member service, cards, details, token interceptor and age pipe

---
+++ client/src/core/pipes/age-pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'age',
+})
+export class AgePipe implements PipeTransform {
+  transform(value: string): number {
+    const today = new Date();
+    const dateOfBirth = new Date(value);
+
+    let age = today.getFullYear() - dateOfBirth.getFullYear();
+    const monthDifference = today.getMonth() - dateOfBirth.getMonth();
+
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < dateOfBirth.getDate())) {
+      age--;
+    }
+
+    return age;
+  }
+}
--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,9 +1,10 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
 import { LoginCredentials } from '../../types/user';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ToastService } from '../../core/services/toast-service';
+import { themes } from '../themes';
 
 @Component({
   selector: 'app-nav',
@@ -11,11 +12,25 @@ import { ToastService } from '../../core/services/toast-service';
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
-export class Nav {
+export class Nav implements OnInit {
   protected accountService = inject(AccountService);
   private toast = inject(ToastService);
   private router = inject(Router);
   protected credentials: Partial<LoginCredentials> = {};
+  protected selectedTheme = signal<string>(localStorage.getItem('theme') || 'light');
+  protected themes = themes;
+
+  ngOnInit(): void {
+    document.documentElement.setAttribute('data-theme', this.selectedTheme());
+  }
+
+  handleSelectTheme(theme: string) {
+    this.selectedTheme.set(theme);
+    localStorage.setItem('theme', theme);
+    document.documentElement.setAttribute('data-theme', theme);
+    const element = document.activeElement as HTMLDivElement;
+    if (element) element.blur();
+  }
 
   login() {
     this.accountService.login(this.credentials as LoginCredentials).subscribe({
